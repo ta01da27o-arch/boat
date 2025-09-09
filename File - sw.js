d856aea540536d97f3cfb650bc306ff283838b2a
ta01da27o-arch <ta01da27o@gmail.com>
2025-09-09 10:29:49 +0900

    Delete File - sw.js

--- File - sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker: Installed");
-});
-self.addEventListener("activate", e => {
-  console.log("Service Worker: Activated");
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
