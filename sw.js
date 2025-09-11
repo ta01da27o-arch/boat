7be9049494fcc5b108222863b6c304e600be07ce
ta01da27o-arch <ta01da27o@gmail.com>
2025-09-11 17:46:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,22 @@
-const CACHE_NAME = 'pwa-test-v1';
-const FILES_TO_CACHE = [
-  './index.html',
-  './style.css',
-  './script.js',
-  './data.json'
-];
-
-// インストール時にキャッシュ
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("app-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./data.json",
+        "./manifest.json"
+      ]);
     })
   );
 });
 
-// オフライン対応
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
