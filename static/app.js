906f92a2a76cb7d098048695f00285b637706d2a
ta01da27o-arch <ta01da27o@gmail.com>
2025-10-20 06:23:07 +0900

    Delete static/app.js

--- static/app.js
+++
@@ -1,8 +0,0 @@
-async function loadData() {
-  const res = await fetch("/data");
-  const data = await res.json();
-  document.getElementById("dataView").textContent = JSON.stringify(data, null, 2);
-}
-
-document.getElementById("refreshBtn").addEventListener("click", loadData);
-loadData();
